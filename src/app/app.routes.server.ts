bcb32e003603f9402be94dc0639997db2cba8ed5
Amani Bisimwa <drcmindfrench@gmail.com>
2025-01-22 17:54:08 +0200

    reverse all change

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,15 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '',
+    path: '**',
     renderMode: RenderMode.Prerender,
-  },
-  {
-    path: 'products/:category',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'product/:id',
-    renderMode: RenderMode.Client,
   },
 ];
